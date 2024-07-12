887cbad9e32d5d94b266711c919dc0bf5dd272ef
Caloy1218 <jcsaquing1218@gmail.com>
2024-07-12 22:01:42 +0800

    1

--- Client/jcf-fitness/src/components/Transformation/Transformation.jsx
+++ Client/jcf-fitness/src/components/Transformation/Transformation.jsx
@@ -1,12 +1,21 @@
 // src/components/Transformations.js
 import React from 'react';
-import { Typography } from '@mui/material';
-
+import back from '../Assets/Transformation/back.png';
+import backfat from '../Assets/Transformation/backfat.jpg';
+import frontpic from '../Assets/Transformation/frontpic.png';
+import frontfat from '../Assets/Transformation/frontfat.jpg';
+import side from '../Assets/Transformation/side.png';
+import sidefat from '../Assets/Transformation/sidefat.jpg';
+import sideback from '../Assets/Transformation/sideback.png';
+import sidebackfat from '../Assets/Transformation/sidebackfat1.jpg';
+import './Transformation.css';
 const Transformations = () => {
   return (
-    <div>
-      <Typography variant="h4">Transformations</Typography>
-      <Typography variant="body1">See the amazing transformations.</Typography>
+    <div className='transformation-container'>
+      <div className="transformation-title">
+        <h1>My six-month transformation took place in 2020. The top picture was taken in April 2020, and the bottom picture was taken in September 2020. </h1>
+        <p>The key to achieving these results was following a meal plan with adjusted daily calories and macronutrients, along with a fully tailored workout </p>
+      </div>
     </div>
   );
 };
